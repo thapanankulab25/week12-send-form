83d5e35e04aa53cead24dbbcd07cf6dbea861c79
thapanankulab25 <s6503052422108@email.kmutnb.ac.th>
2024-03-04 09:33:03 +0700

    update

---
+++ app1/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import FormSearch  from './form-search';
+import FormBootstrap from './form-bootstrap'
+import FormText from './form-text';
+
+export default function App() {
+
+  return <>
+  <FormSearch/>
+  <br>
+  </br>
+  <FormBootstrap/>
+  <br>
+  </br>
+  <FormText/>
+  </>
+}
