4c7e44ddfa67eb23bd26fb9a46709c6da1b073ec
thapanankulab25 <s6503052422108@email.kmutnb.ac.th>
2024-03-04 10:58:34 +0700

    update

--- app1/src/App.js
+++ app1/src/App.js
@@ -8,6 +8,6 @@ export default function App() {
  
 
   return <>
-  <FormCheck/> <br></br><FormSelectFile/> <br></br> <FormValidation/>
+  <FormCheck/><br></br><FormSelectFile/> <br></br> <FormValidation/>
   </>
 }
